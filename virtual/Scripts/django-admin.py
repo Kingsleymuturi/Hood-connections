08f8dbe1b8801011e3eb5de260d9c392b3bb2fec
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-18 12:01:26 +0300

    remove virtual

--- virtual/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\desktop\python\hello_django\hood-connections\virtual\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
