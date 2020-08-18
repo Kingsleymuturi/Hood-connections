68fb3ce48dd8ce8c53dd450bfe8f97842be050f8
sarahsindet <sarahsindet@gmail.com>
2020-08-18 15:21:19 +0300

    added an image

--- hoodie/forms.py
+++ hoodie/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, NeighbourHood,Post, Business 
+from .models import Profile, NeighbourHood, Post, Business 
 from pyuploadcare.dj.forms import ImageField
 
 class SignupForm(UserCreationForm):
