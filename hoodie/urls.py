6a7ebdd75d2b06bb111c7e009b968380a5093330
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-18 11:07:10 +0300

    add urls for user profile and authentication

---
+++ hoodie/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+]
