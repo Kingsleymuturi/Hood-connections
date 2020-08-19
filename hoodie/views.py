52a7edf41049a1c3f1d898d6f7f67afbfb240cc5
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-19 16:50:20 +0300

    add newhood template

--- hoodie/views.py
+++ hoodie/views.py
@@ -55,3 +55,16 @@ def create_hood(request):
     else:
         form = NeighbourHoodForm()
     return render(request, 'newhood.html', {'form': form})
+
+def join_hood(request, id):
+    neighbourhood = get_object_or_404(NeighbourHood, id=id)
+    request.user.profile.neighbourhood = neighbourhood
+    request.user.profile.save()
+    return redirect('hood')
+
+
+def leave_hood(request, id):
+    hood = get_object_or_404(NeighbourHood, id=id)
+    request.user.profile.neighbourhood = None
+    request.user.profile.save()
+    return redirect('hood')
