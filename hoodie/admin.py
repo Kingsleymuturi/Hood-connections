046995f6c11d265f2df95fc5f9c2bf1d40ef898e
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-08-19 15:16:57 +0300

    add business and post models to admin

--- hoodie/admin.py
+++ hoodie/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Profile, NeighbourHood
+from .models import Profile, NeighbourHood, Business, Post
+
 
 admin.site.register(Profile)
 admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)
