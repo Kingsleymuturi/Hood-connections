98ce6dc6e99774b362fed85ebb83284c0f0717e2
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-21 00:16:18 +0300

    added url  to update profile

--- hoodie/api/urls.py
+++ hoodie/api/urls.py
@@ -6,6 +6,8 @@ from hoodie.api.views import(
     create_hood,
     update_hood,
     delete_hood,
+    HoodListView,
+    update_profile
 ) 
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -16,7 +18,9 @@ urlpatterns = [
     path('v1/hoods/<int:pk>/update_hood/', update_hood, name='update-hood'),
     path('v1/hoods/<int:pk>/delete_hood/', delete_hood, name='delete-update'),
     path('v1/hoods/', hoods, name='hoods'),
-    path('view_hood/', view_hood, name='hood_info'),
+    path('v1/hoods_list/', HoodListView.as_view(), name='hoods_list'),
+    path('v1/hoods/<int:pk>/view_hood/', view_hood, name='hood_info'),
     path('auth/register/', registration_view, name='register'),
     path('auth/login/',obtain_auth_token, name='login'),
+    path('v1/<int:pk>/profile/',update_profile, name='update_profile'),
 ]
