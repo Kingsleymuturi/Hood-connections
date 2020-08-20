73a6a39f792d8371ab9a2fb22037e110a8dd3250
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-21 00:15:53 +0300

    added Pagination class

--- hoodconnection/settings.py
+++ hoodconnection/settings.py
@@ -81,7 +81,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':1,
 }
 
 # AUTH_USER_MODEL = 'hoodconnection.Profile'
