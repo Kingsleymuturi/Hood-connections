ee8c4102356c9a5434463541f6a8af0cfd7bf77e
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-20 15:02:08 +0300

    added TokenAuthentication

--- hoodconnection/settings.py
+++ hoodconnection/settings.py
@@ -74,6 +74,12 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
